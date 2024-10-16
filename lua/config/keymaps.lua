6acd9ada082fff3d62d337bbff75b484560075a5
Jan Dolenc <jan.dolenc9@gmail.com>
2024-10-16 20:59:46 +0200

    leave insert mode with jk - <esc>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- making changes
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Remap <Esc> to jk - go to normal mode" })
